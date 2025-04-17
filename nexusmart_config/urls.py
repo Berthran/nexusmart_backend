1b2c35a56f6ed60b4b9d400bd856a8bcd2088a94
Daniel Berthran <berthlearn@gmail.com>
2025-04-17 11:47:19 +0100

    Add JWT Token URLS in nexusmart_config/urls.py

--- nexusmart_config/urls.py
+++ nexusmart_config/urls.py
@@ -1,33 +1,29 @@
-"""
-URL configuration for nexusmart_config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     # Default Django admin site URL
     path('admin/', admin.site.urls),
 
-    # Include the URLs from the 'products' app under the 'api/v1' prefix
-    # Remove the namespace argument as the router handles URL naming.
+    # --- API V1 URLS
+
     path(
         'api/v1/', # Prefix remains the same
         include('products.urls') # Just include the module path
     ),
 
+    # --- JWT Authentication URLs ---
+    # Endpoint to obtain token pair (access and refresh) by posting username/password
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # Endpoint to obtain a new access token by posting a valid refresh token
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # We can add URLs for other apps (like users, orders) here later
     # path('api/v1/users/', include('users.urls')), # Example for later
 ]
 
