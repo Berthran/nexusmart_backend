d81c6a35e3851be5bb414ed23106432ef4fbf878
Daniel Berthran <berthlearn@gmail.com>
2025-06-12 14:30:40 +0100

    Serve Media Files in Development in nexusmart_config/urls.py

--- nexusmart_config/urls.py
+++ nexusmart_config/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
 # Add configurations for serving media files during development later if needed
 # from django.conf import settings
 # from django.conf.urls.static import static
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
