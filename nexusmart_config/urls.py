22912a5efdb7558620d047cf5559d56b193a643f
Daniel Berthran <berthlearn@gmail.com>
2025-06-12 14:23:03 +0100

    Add drf-spectacular URLS in nexusmart_config/urls.py

--- nexusmart_config/urls.py
+++ nexusmart_config/urls.py
@@ -1,30 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# For serving medial files during development
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from drf_spectacular.views import (
+    SpectacularAPIView,      # Serves the raw OpenAPI schema file
+    SpectacularSwaggerView,  # Serves the Swagger UI interactive documentation
+    SpectacularRedocView,    # Serves the ReDoc documentation UI
+)
+
+
 urlpatterns = [
-    # Default Django admin site URL
     path('admin/', admin.site.urls),
-
-    # --- API V1 URLS
-
-    path(
-        'api/v1/', # Prefix remains the same
-        include('products.urls') # Just include the module path
-    ),
-
-    # --- JWT Authentication URLs ---
-    # Endpoint to obtain token pair (access and refresh) by posting username/password
+    path('api/v1/', include('products.urls')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # Endpoint to obtain a new access token by posting a valid refresh token
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-    # We can add URLs for other apps (like users, orders) here later
-    # path('api/v1/users/', include('users.urls')), # Example for later
 ]
 
 
